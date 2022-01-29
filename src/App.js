416868abd10ae7bdb622cdd946bd748d8b0c6c11
rkaufusi <rhettkaufusi@gmail.com>
2022-01-28 19:53:34 -0800

    init, added appBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { create } from '@mui/material/styles/createTransitions';
+import {useState} from 'react'
 import './App.css';
 
-function App() {
+function App({toDo, data}) {
+
+console.log(data.join(' '))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Sort Visualizer</h3>
+      <p>{data.join(' ')}</p>
     </div>
   );
 }
